fd1265a613c4033a3d8c7b27019b5340fad8822d
Theresa <theresaobamwonyi@gmail.com>
2021-09-25 09:32:32 +0100

    implemented js for cart summation

--- cart/static/js/app.js
+++ cart/static/js/app.js
@@ -1,22 +1,37 @@
 
-function calculateSingleItemPrice() {
-  var itemQuantity = document.getElementById("potatoQty");
-  var itemPrice = document.getElementById("potatoPrice");
-  var itemPrice = parseFloat(itemPrice.value);
-  var itemQuantity = parseFloat(itemQuantity.value);
-  var totalPrice = itemPrice * itemQuantity;
-  document.getElementById("singleItemTotal").value = totalPrice;
+function calculateSingleItemPrice(e) {
+  if (e == 'potatoes'){
+    var itemQuantity = document.getElementById("potatoQty");
+    var itemPrice = document.getElementById("potatoPrice");
+    var totalId = 'potatoTotal';
+  }
+
+  if (e == 'carrots'){
+    var itemQuantity = document.getElementById("carrotQty");
+    var itemPrice = document.getElementById("carrotPrice");
+    var totalId = 'carrotTotal';
+  }
+
+  if (e == 'onions'){
+    var itemQuantity = document.getElementById("onionQty");
+    var itemPrice = document.getElementById("onionPrice");
+    var totalId = 'onionTotal';
+  }
+  
+  var itemPrice = parseFloat(itemPrice.value) || 0;
+  var itemQuantity = parseFloat(itemQuantity.value) || 0;
+  var totalPrice = itemPrice * itemQuantity || 0;
+
+  document.getElementById(totalId).value = totalPrice;
+  calculateSubTotal();
 }
 
 function calculateSubTotal(){
-  var grandTotal = 0.0
-  var table = document.getElementById('cart-table');
-    for (var r = 0, n = table.rows.length; r < n; r++) {
-        for (var c = 0, m = table.rows[r].cells.length; c < m; c++) {
-            // return table.rows[r].cells[c].innerHTML;
-            var singleItemTotal = document.getElementById("singleItemTotal").value;
-            grandTotal += singleItemTotal;
-        }
-    }
-    document.getElementById("total").value = totalPrice;
+  var  potatoTotal = parseFloat(document.getElementById("potatoTotal").value) || 0;
+  var  carrotTotal = parseFloat(document.getElementById("carrotTotal").value) || 0;
+  var  onionTotal = parseFloat(document.getElementById("onionTotal").value) || 0;
+
+  var grandTotal = potatoTotal + carrotTotal + onionTotal;
+  document.getElementById("total").value = grandTotal;
+
 }
