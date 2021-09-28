bdfb368e8b5df411884d4c0afdec51d598796c5b
Theresa <theresaobamwonyi@gmail.com>
2021-09-28 21:33:35 +0100

    formatted with prettier

--- cart/static/js/app.js
+++ cart/static/js/app.js
@@ -1,18 +1,22 @@
+function calculateSingleItemPrice() {
+  var cartRows = document.getElementsByClassName("cart-item");
+  var subTotal = 0;
 
-function calculateSingleItemPrice() { 
-  var cartRows = document.getElementsByClassName("cart-item")
-  var subTotal = 0
+  for (var i = 0; i < cartRows.length; i++) {
+    var itemQuantity =
+      parseInt(
+        cartRows[i].getElementsByClassName("currentProductQuantity")[0].value
+      ) || 0;
+    var itemPrice =
+      parseFloat(
+        cartRows[i].getElementsByClassName("currentProductPrice")[0].value
+      ) || 0;
 
-  for(var i=0; i < cartRows.length; i++){
-
-      var itemQuantity = parseInt(cartRows[i].getElementsByClassName("currentProductQuantity")[0].value) || 0;
-      var itemPrice = parseFloat(cartRows[i].getElementsByClassName("currentProductPrice")[0].value) || 0;
-
-      var totalPrice = itemQuantity * itemPrice
-      cartRows[i].getElementsByClassName("currentProductTotal")[0].value = totalPrice;
-      subTotal += totalPrice;
-      document.getElementById("total").value = subTotal;
-
+    var totalPrice = itemQuantity * itemPrice;
+    cartRows[i].getElementsByClassName("currentProductTotal")[0].value =
+      totalPrice;
+    subTotal += totalPrice;
+    document.getElementById("total").value = subTotal;
   }
 }
 
